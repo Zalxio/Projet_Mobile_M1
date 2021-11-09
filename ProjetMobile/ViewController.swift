3d029ed21b338a67874fdcc2c8480be90c2852c6
Zalxio <dyl_974@icloud.com>
2021-11-09 08:32:34 +0400

    Initial Commit

---
+++ ProjetMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjetMobile
+//
+//  Created by Dylan Cherrier on 09/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
